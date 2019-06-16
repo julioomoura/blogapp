23132c2b8f500682b544216072d425bbbe15a247
Júlio Moura <juliomourajom@gmail.com>
2019-06-16 15:29:43 -0300

    Definindo o model de categorias

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
     const app = express()
     const admin = require('./routes/admin')
     const path = require("path")
-    //const mongoose = require('mongoose')
+    const mongoose = require('mongoose')
 
 //Configurações
     //Body Parser
@@ -15,7 +15,12 @@
         app.engine('handlebars', handlebars({defaultLayout : 'main'}))
         app.set('view engine', 'handlebars')
     //Mongoose
-        //
+        mongoose.Promise = global.Promise
+        mongoose.connect("mongodb://localhost/blogapp").then(() => {
+            console.log("Conectado ao mongo")
+        }).catch((erro) => {
+            console.log("Erro ao se conectar: " + erro)
+        })
     //Public
         app.use(express.static(path.join(__dirname, "public")))
 //Rotas
