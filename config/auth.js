54dd3b4c2cb131c8ee1fdc12a389ed396f80a6a2
Julio <juliomourajom@gmail.com>
2019-07-02 15:21:26 -0300

    Finalizando autenticação

--- config/auth.js
+++ config/auth.js
@@ -7,7 +7,7 @@ require('../models/Usuario')
 const Usuario = mongoose.model("usuarios")
 
 module.exports = function(passport){
-    passport.use(new localStrategy({usernameField: "email"}, (email, senha, done) => {
+    passport.use(new localStrategy({usernameField: "email", passwordField: "senha"}, (email, senha, done) => {
         Usuario.findOne({email:email}).then((usuario) => {
             if(!usuario){
                 return done(null, false, {message: "Esta conta não existe"})
@@ -15,7 +15,7 @@ module.exports = function(passport){
 
             bcrypt.compare(senha, usuario.senha, (erro, batem) => {
                 if(batem){
-                    return done(null, user)
+                    return done(null, usuario)
                 }else{
                     return done(null, false, {message: "Senha incorreta"})
                 }
@@ -29,7 +29,7 @@ module.exports = function(passport){
 
     passport.deserializeUser((id, done) => {
         Usuario.findById(id, (erro, usuario) => {
-            done(err, usuario)
+            done(erro, usuario)
         })
     })
 }
